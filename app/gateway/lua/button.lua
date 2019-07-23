4d2948195e98cf0c3d03f89416863ae9c945d032
Zhen Cheng <M084702@saedgo1.sae.com.hk>
2019-07-23 15:36:10 +0800

    button

--- app/gateway/lua/button.lua
+++ app/gateway/lua/button.lua
@@ -2,49 +2,54 @@ local path = require("path")
 local fs = require("fs")
 local wot = require("wot")
 
-
-local SYS_CONFIG = 5
-local SYS_RESET = 8
+local TIMEOUT_NETWORK_RESET = 4
+local TIMEOUT_SYSTEM_RESET = 10
 local DEFAULT_IP = "192.168.8.2"
 
 local exports = {}
 local pressTime = 0
-local function CheckButtonState(interval_ms)
-    console.log("button check")
-    setInterval(
-        interval_ms,
-        function()
-            local path = "/sys/class/gpio/gpio62/value"
-            local source = fs.openSync(path, "r", 438)
-            if (not source) then
-                return
-            end
+local networkReset = 0
+local systemReset = 0
 
-            local result = fs.readSync(source)
-            fs.closeSync(source)
-            state = tonumber(string.match(result, "(%d)\n"))
-            if (state == 0) then
-                pressTime = pressTime + 1
-                console.log(pressTime)
-            end
-
-            if (state == 1) then
-                if (pressTime > SYS_CONFIG and pressTime < SYS_RESET) then
-                    console.log("set ip:" .. DEFAULT_IP)
-                    io.popen("ifconfig eth0 192.168.8.104")
-                   
-                elseif (pressTime > SYS_RESET) then
-                    console.log("sys reset")
-                end
-                pressTime = 0
-            end
-
-            -- console.log(state)
+local function checkButtonStatus(interval_ms)
+    setInterval(interval_ms, function()
+        local filename = "/sys/class/gpio/gpio62/value"
+        local filedata, err = fs.readFileSync(filename)
+        if (not filedata) then
+            console.log('checkButtonStatus', err)
+            return
         end
-    )
+
+        local state = tonumber(filedata)
+        -- console.log('state', state, filedata)
+
+        if (state == 0) then
+            pressTime = pressTime + 1
+            print('reset button down', pressTime)
+
+           if (pressTime >= TIMEOUT_SYSTEM_RESET) then
+                if (systemReset ~= 1) then
+                    systemReset = 1
+                    console.log("system reset")
+                end
+
+            elseif (pressTime >= TIMEOUT_NETWORK_RESET) then
+                if (networkReset ~= 1) then
+                    networkReset = 1
+                    console.log("network reset:", DEFAULT_IP)
+                    -- os.execute("ifconfig eth0 192.168.8.104")
+                end
+            end
+
+        else
+            pressTime = 0
+            networkReset = 0
+            systemReset = 0
+        end
+    end)
 end
 
-local function ledSwitch(color, state)
+local function setLEDStatus(color, state)
     local value
     local led = {
         green = "/sys/class/gpio/gpio53/value",
@@ -72,17 +77,13 @@ local function ledSwitch(color, state)
         return
     end
     
-    fs.write(
-        fd,
-        nil,
-        value,
-        function(err, written)
-            fs.closeSync(fd)
-        end
-    )
+    fs.write(fd, nil, value, function(err, written)
+        fs.closeSync(fd)
+    end)
 end
 
-exports.checkButton = CheckButtonState
+exports.checkButton = checkButtonStatus
 exports.runningLed = runningStateindex
-exports.ledSwitch = ledSwitch
+exports.ledSwitch = setLEDStatus
+
 return exports
